9ac6d6f0266398dd604edee2ba8dd5aad6861490
Fusa Vladislav <fusavlad@gmail.com>
2024-01-18 15:08:05 +0200

    Added get and post request

--- index.js
+++ index.js
@@ -2,11 +2,42 @@ const express = require("express");
 
 const app = express();
 
-app.use(()=>{
-    console.log("We got a request!")
+// app.use((req, res) => {
+//     console.log("We got a request!")
+//     // res.send("We got your request! this is a responce!")
+//     // res.send({ color: "red", name: "Blue", age: "54" })
+//     res.send("<h1>This is a responce H1</h1>")
+// })
+app.get("/", (req, res) => {
+    res.send("home page!")
 })
 
+app.get("/r/:subreddit", (req, res) => {
+    const { subreddit } = req.params;
+    res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`)
+})
 
-app.listen(3000, ()=>{
+app.get("/r/:subreddit/:postId", (req, res) => {
+    const { subreddit, postId } = req.params;
+    res.send(`<h1>Browsing the ${subreddit} subreddit</h1> with the id of ${postId}`)
+})
+
+app.get("/cats", (req, res) => {
+    console.log("cat request!");
+    res.send("meow!")
+})
+
+app.post("/cats", (req, res) => {
+    res.send("Request was posted on /cats")
+})
+app.get("/dogs", (req, res) => {
+    console.log("dog request!");
+    res.send("bark!")
+})
+
+app.get("*", (req, res) => {
+    res.send("Undentified request!")
+})
+app.listen(3000, () => {
     console.log("Port: 3000")
 })
